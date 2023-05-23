56c1a314d5b0b19ec42b6ed82f1bd0291d66156c
edydex <97430334+edydex@users.noreply.github.com>
2023-05-23 12:20:40 -0700

    update1

--- script.js
+++ script.js
@@ -7,14 +7,34 @@ function renderLinks() {
   linkList.innerHTML = '';
 
   links.forEach(function (link) {
-    const listItem = document.createElement('li');
-    listItem.innerHTML = `<a href="${link.url}">${link.name}</a>`;
-    linkList.appendChild(listItem);
+    const linkItem = document.createElement('div');
+    linkItem.classList.add('link-item');
+
+    const heading = document.createElement('h2');
+    heading.textContent = link.name;
+    linkItem.appendChild(heading);
+
+    const content = document.createElement('div');
+    content.classList.add('link-content');
+
+    const linkAnchor = document.createElement('a');
+    linkAnchor.href = link.url;
+    linkAnchor.textContent = 'Link';
+    content.appendChild(linkAnchor);
+
+    const description = document.createElement('p');
+    description.textContent = link.description;
+    content.appendChild(description);
+
+    linkItem.appendChild(content);
+    linkList.appendChild(linkItem);
+
+    // Add event listener to toggle the content visibility
+    heading.addEventListener('click', function () {
+      content.classList.toggle('show');
+    });
   });
 }
 
-// Function to handle the form submission
-function handleSubmit(event) {
-  event.preventDefault();
-  const nameInput = document.getElementById('link-name');
-  const urlInput = document.getElementById('link-url
+// Call the renderLinks function initially
+renderLinks();
