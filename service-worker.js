5cfb05024f983a571b47c4544ffc68abde01b7e0
edydex <97430334+edydex@users.noreply.github.com>
2025-02-26 15:57:42 -0800

    Dune PWA attempt 1

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+const CACHE_NAME = "dune-tracker-v1";
+const urlsToCache = [
+    "./dune.html",
+    "./favicon.jpg",
+    "./manifest.json",
+    "./dune_x1024.png"
+];
+
+self.addEventListener("install", event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => cache.addAll(urlsToCache))
+    );
+});
+
+self.addEventListener("fetch", event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener("activate", event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
